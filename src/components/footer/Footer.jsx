98d9fd9796b7a7747a9319eed5c2b23f7f6505e6
Rishav Jaiswal <rishavjaiss14@gmail.com>
2022-10-05 13:09:19 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = ({ length }) => {
   return (
-    <footer className="absolute bottom-0 bg-green-300 w-full p-3 text-center font-semibold">
+    <footer className="absolute bottom-0 bg-green-300 w-full p-3 text-center font-semibold text-orange-600">
       {length} {length === 1 ? 'List item' : 'List items'}
     </footer>
   );
