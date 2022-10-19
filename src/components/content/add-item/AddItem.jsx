66fce1f21164e4128b1fa85424aa75c735450518
yahoogm <yaho.garfield@gmail.com>
2022-10-19 08:25:34 +0700

    update UI

--- src/components/content/add-item/AddItem.jsx
+++ src/components/content/add-item/AddItem.jsx
@@ -6,9 +6,19 @@ const AddItem = ({ handleSubmit, newItem, setNewItem }) => {
   const inputRef = useRef();
   return (
     <form className="flex items-center justify-center mt-20 space-x-2" onSubmit={handleSubmit}>
-      <input autoFocus id="addItem" type="text" placeholder="Add Item" required className="bg-green-300 rounded p-2 font-mono placeholder:text-white" value={newItem} onChange={(e) => setNewItem(e.target.value)} ref={inputRef} />
-      <button type="submit" aria-label="Add Item" onClick={() => inputRef.current.focus()}>
-        <FaPlus type="submit" aria-label="Add Item" onClick={() => inputRef.current.focus()} />
+      <input
+        autoFocus
+        id="addItem"
+        type="text"
+        placeholder="Add Item"
+        required
+        className="bg-green-300 rounded p-2 font-mono placeholder:text-white outline-none shadow-xl"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+        ref={inputRef}
+      />
+      <button type="submit" className="bg-green-300 p-3 rounded-md  shadow-xl" aria-label="Add Item" onClick={() => inputRef.current.focus()}>
+        <FaPlus type="submit" className="text-white" aria-label="Add Item" onClick={() => inputRef.current.focus()} />
       </button>
     </form>
   );
--- src/components/content/search/Search.jsx
+++ src/components/content/search/Search.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const search = ({ search, setSearch }) => {
   return (
     <form className="flex justify-center mt-10" onSubmit={(e) => e.preventDefault()}>
-      <input id="search" type="text" role="searchbox" placeholder="Search Items" className="bg-green-300 rounded p-2 font-mono placeholder:text-white" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <input id="search" type="text" role="searchbox" placeholder="Search Items" className="bg-green-300 rounded p-2 font-mono placeholder:text-white outline-none shadow-xl" value={search} onChange={(e) => setSearch(e.target.value)} />
     </form>
   );
 };
